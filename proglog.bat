1ccda7b28a8d75b08fa66893438e15cd03b37656
jshores281 <52839097+jshores281@users.noreply.github.com>
2023-07-09 00:43:15 -0500

    updated: added directory setting feature, organized total script into menu based term-app, tested with local NAS, made local and central (nas) proglog entries)

--- proglog.bat
+++ proglog.bat
@@ -1,50 +1,296 @@
 @echo off
-echo WELCOME TO YOUR NEW DAILY PROGRESS LOGGER
+goto :main
+:: add if new month (creates new file with month name and year, set persistent env var of month/year)
+
+
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:main
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo Development Progress Logging
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo.
+echo  _____  _____   ____   _____ _      ____   _____
+echo ^|  __ \^|  __ \ / __ \ / ____^| ^|    / __ \ / ____^|
+echo ^| ^|__) ^| ^|__) ^| ^|  ^| ^| ^|  __^| ^|   ^| ^|  ^| ^| ^|  __
+echo ^|  ___/^|  _  /^| ^|  ^| ^| ^| ^|_ ^| ^|   ^| ^|  ^| ^| ^| ^|_ ^|
+echo ^| ^|    ^| ^| \ \^| ^|__^| ^| ^|__^| ^| ^|___^| ^|__^| ^| ^|__^| ^|
+echo ^|_^|    ^|_^|  \_\\____/ \_____^|______\____/ \_____^|
+echo.                                                 
 echo.
-echo enter all productivities into this program to review progress later
-:: add option to make new temp location for prog log
+
+goto :menu
+
+
+:dir_set
+echo Current dir: %proglog_dir%
+echo.
+echo Set proglog dir.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo 1. Local  
+echo 2. Remote
+echo 3. Manual
+echo 4. skip
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+set /p dir="> "
+
+
+if %dir%==1 (
+  echo Local directory was selected
+  set proglog_dir="%userprofile%\Documents\proglog_CURRENT.txt"
+)
+
+:: do a connection check
+if %dir%==2 (
+  echo Remote directory was selected  
+  set proglog_dir="\\remote-server\proglog072023.txt"
+)
+
+if %dir%==3 (
+  echo Manual directory was selected
+  echo Type path where to store proglog:
+  set /p proglog_dir="> "
+)
+
+if %dir%==4 (
+  goto :menu
+)
+
+goto :menu
+
+
+:menu
+echo.
+echo Current dir: %proglog_dir%
+echo.
+echo Choose from the menu.
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo 0. Set directory
+echo 1. Add a time_piece note  
+echo 2. Open time_piece notes
+echo 3. Read all time_piece notes
+echo 4. Create a new time_piece file
+echo 5. Exit
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+set /p choice="> "
+
+if %choice%==0 goto :dir_set
+if %choice%==1 goto :note
+if %choice%==2 goto :editor
+if %choice%==3 goto :reader
+if %choice%==4 goto :New_file
+if %choice%==5 goto :EO
+goto :EO
 
 
 
-:: default_location
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:note
+setlocal EnableDelayedExpansion
+echo.
+echo Enter notes:
+set /p proglog=" "
 
-:: cd "C:\Users\%username%\Documents\NOTES"
-echo. >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
+echo ------------------------------------- >> %proglog_dir%
+echo last working directory: >> %proglog_dir%
+echo %cd% >> %proglog_dir%
+echo %date% >> %proglog_dir%
+echo %time% >> %proglog_dir%
+echo. >> %proglog_dir%
+echo !proglog! >> %proglog_dir%
+echo. >> %proglog_dir%
+echo. >> %proglog_dir%
+echo ------------------------------------- >> %proglog_dir%
 echo.
-echo.
-echo ---------------------------------------------------------- >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-echo ---------------------------------------------------------- >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-echo %cd% >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-echo.
-echo.
-echo.
-echo %date% >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-echo %time% >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-echo.
-echo.
-echo. >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-:: ADDING NEW INPUTS AT TOP OF TXT FILE
-::call notepad dailylog.txt
-:: copy existing file to a temporary file
-::copy c:\temp\existing.txt c:\temp\temp.txt
-:: replace content with your new value
-::echo test >c:\temp\existing.txt
-::  append content of temp file
-::for /F %%i in (c:\temp\temp.txt) do echo %%i >> c:\temp\existing.txt
-:: remove tempfile
-::del c:\temp\temp.txt
+setlocal DisableDelayedExpansion
+echo Entry saved!
 
-set /p proglog="enter what you did: "
-echo %proglog% >> "C:\Users\%username%\Documents\NOTES\proglog.txt"
-notepad "C:\Users\%username%\Documents\NOTES\proglog.txt"
-:: make sentense breaks
-::if %dailylog% == %dailylog% echo %dailylog% >> dailylog.txt
+goto :add_note
 
-::log checker
-echo entry has been logged
-echo. 
+
+
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:editor
 echo.
-echo goodnight
-echo. 
+set /p choice="Open editor? [y]/[n]: "
+:: ADD ERROR HANDLING FEATURE
+if %choice% == y (
+  sublime %proglog_dir%
+  goto :add_note
+) else (
+  goto :main
+)
+ 
+
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:reader
+type %proglog_dir%
+pause
+goto :main
+
+
+
+
+
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:add_note
 echo.
+set /p loopend="Add another note? [y]/[n]: "
+
+if %loopend% == y (
+  goto :note
+) else (
+  goto :main
+)
+
+
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:New_file
+echo.
+echo yet to be implemented
+echo.
+pause
+goto :main
+
+
+ 
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:EO
+echo.
+echo Have a good time, Joshua!
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rem @echo off
+rem goto :main
+rem :: add if new month (creates new file with month name and year, set persistent env var of month/year)
+
+
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :main
+rem echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem echo Development Progress Logging
+rem echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem echo.
+rem echo  _____  _____   ____   _____ _      ____   _____
+rem echo ^|  __ \^|  __ \ / __ \ / ____^| ^|    / __ \ / ____^|
+rem echo ^| ^|__) ^| ^|__) ^| ^|  ^| ^| ^|  __^| ^|   ^| ^|  ^| ^| ^|  __
+rem echo ^|  ___/^|  _  /^| ^|  ^| ^| ^| ^|_ ^| ^|   ^| ^|  ^| ^| ^| ^|_ ^|
+rem echo ^| ^|    ^| ^| \ \^| ^|__^| ^| ^|__^| ^| ^|___^| ^|__^| ^| ^|__^| ^|
+rem echo ^|_^|    ^|_^|  \_\\____/ \_____^|______\____/ \_____^|
+rem echo.                                                 
+rem echo.
+rem set proglog_dir="C:\Users\%username%\Documents\0-NOTES\BULBS\proglog_CURRENT.txt"
+
+rem echo Current dir: %proglog_dir%
+rem echo.
+rem echo Choose from the menu.
+rem echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem echo 1. Add a time_piece note
+rem echo 2. Open time_piece notes
+rem echo 3. Read all time_piece notes
+rem echo 4. Create a new time_piece file
+rem echo 5. Exit
+rem echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem echo.
+rem set /p choice="> "
+
+rem if %choice%==1 goto :note
+rem if %choice%==2 goto :editor
+rem if %choice%==3 goto :reader
+rem if %choice%==4 goto :New_file
+rem if %choice%==5 goto :EO
+rem goto :EO
+
+
+
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :note
+rem setlocal EnableDelayedExpansion
+rem echo.
+rem echo Enter notes:
+rem set /p proglog=" "
+rem echo ------------------------------------- >> %proglog_dir%
+rem echo last working directory: >> %proglog_dir%
+rem echo %cd% >> %proglog_dir%
+rem echo %date% >> %proglog_dir%
+rem echo %time% >> %proglog_dir%
+rem echo. >> %proglog_dir%
+rem echo !proglog! >> %proglog_dir%
+rem echo. >> %proglog_dir%
+rem echo. >> %proglog_dir%
+rem echo ------------------------------------- >> %proglog_dir%
+rem echo Entry saved!
+rem goto :add_note
+
+
+
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :editor
+rem echo.
+rem set /p choice="Open editor? [y]/[n]: "
+
+rem if %choice% == y (
+rem   sublime %proglog_dir%
+rem   goto :add_note
+rem ) else (
+rem   goto :main
+rem )
+ 
+
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :reader
+rem type %proglog_dir%
+rem pause
+rem goto :main
+
+
+
+
+
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :add_note
+rem echo.
+rem set /p loopend="Add another note? [y]/[n]: "
+
+rem if %loopend% == y (
+rem   goto :note
+rem ) else (
+rem   goto :main
+rem )
+
+
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :New_file
+rem echo.
+rem echo yet to be implemented
+rem echo.
+rem pause
+rem goto :main
+
+
+ 
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+rem :EO
+rem echo.
+rem echo Have a good time, Joshua!
+rem ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
